e9ba5d96d7e89a2c12b8eb7f0ca73d071090d420
leofamp <engleofamp@gmail.com>
2024-04-19 18:18:52 -0300

    mod40

---
+++ mod40/src/main/java/com/lmarques/SpringBootProjetoApplication.java
@@ -0,0 +1,14 @@
+package com.lmarques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjetoApplication.class, args);
+		System.out.println("Executando projeto Spring Boot");
+	}
+
+}
