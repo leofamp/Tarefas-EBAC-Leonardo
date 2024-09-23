19893aa95c96e1633483ab9a8f21ea4d79b5f6ea
leofamp <engleofamp@gmail.com>
2024-09-23 03:03:06 -0300

    exercicio mod9

---
+++ Modpro09/src/test/java/com/lmarques/animalservice/AnimalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.lmarques.animalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
